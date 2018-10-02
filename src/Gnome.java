3a91fc2d51cf57314b28eec16746712d79a55c19
looC0reZ <zerocool.btk@gmail.com>
2018-10-02 14:31:12 +0300

    Selection and Gnome

---
+++ src/Gnome.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class Gnome
+{
+    public static void main(String[] arg)
+    {
+        int mas[] = {6, 12, 56, 8, 32, 123, 4, 45, 29, 96, 46, 15, 23, 42, 108};
+        int i = 1;
+        while (i < mas.length)
+        {
+            if (mas[i-1] <= mas[i])
+            {
+                i++;
+            }
+            else
+                {
+                int temp = mas[i-1];
+                mas[i-1] = mas[i];
+                mas[i] = temp;
+                if (i > 1)
+                {
+
+                    i--;
+                }
+            }
+
+        }
+        String result = Arrays.toString(mas);
+        System.out.println(result);
+    }
+
+}
---
+++ src/Selection.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Selection
+{
+    public static void main(String[] arg)
+    {
+        int mas[] = {6, 12, 56, 8, 32, 123, 4, 45, 29, 96, 46, 15, 23, 42, 108, 1};
+        for (int i =0; i < mas.length -1; i++)
+        {
+            int max = Integer.MIN_VALUE;
+            int pos = 0;
+            int sort = mas.length - 1 - i;
+            for (int j = 0; j < mas.length - i; j++)
+            {
+                if (mas[j] > max)
+                {
+                    max = mas[j];
+                    pos = j;
+                }
+            }
+            int temp = mas[sort];
+            mas[sort] = mas[pos];
+            mas[pos] = temp;
+        }
+        String result = Arrays.toString(mas);
+        System.out.println(result);
+    }
+
+}
+
